8a3037bfe332c65f9e34a65e5b49add02d85175f
Surajit Karuri <surajitkaruri@gmail.com>
2021-08-18 02:16:17 +0530

    added mixin, button & some cool anim

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Foodie', desc: 'Tasty pizza!! yumm!!😋' });
+  const dishes = [{imgSrc: '/images/pizza-pic.jpg', imgAlt: 'pizza-image', desc: 'Tasty pizza!! yumm!!😋'},
+                    {imgSrc: '/images/pexels-elli-1854652.jpg', imgAlt: 'choco-pastry', desc: 'Yumm!! so mouth watering 😋'}]
+  res.render('index', { title: 'Foodie', dishes: dishes });
 });
 
 module.exports = router;
